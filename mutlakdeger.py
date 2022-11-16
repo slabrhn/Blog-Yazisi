97d4ef445c6b8e68679d0dc3dc9aeaa7cb7afc4d
SILA BURHAN <90523781+slabrhn@users.noreply.github.com>
2022-11-16 21:14:41 +0300

    Create mutlakdeger.py

---
+++ mutlakdeger.py
@@ -0,0 +1,37 @@
+BLOG YAZISI
+Bu blog yazımda aslında ne kadar blog yazım demek doğru olur bilmiyorum :)). Çünkü bu blog yazısı bana verilen bir proje aslında.
+
+Bu proje de benden istenilen sezgisel öğrenme ile öğrendiğimiz bir matematik konusunda blog yazısı yazmaktı. Merak etmiş olabilirsiniz sezgisel öğrenme nedir, bu proje veren kim yada kimler ?
+
+Hemen açıklayayım o zaman. İlk olarak sezgisel öğrenmeden başlayalım.
+
+Sezgisel Öğrenme bir sorunun çözümünü, zor bir durumdan çıkışı birden ve bütünsel bir kavrayışla bulmaya dayalı bir öğrenmedir. Sezgisel öğrenmeye detaylı bakmak isteyen KariyerRehberlik sayfasına bakabilir.
+
+İkinci olarak açıklamak istediğim konu ise bana bu projeyi yaptıran yada yaptıranlar kimler :)). Bu proje Bootcamp Hızlandırma Eğitimi kapsamında bulunan Temel Matenatik modülününde bizden istenilen projedir. Benim için ilk olacak olan blog yazısı ile tanıştıran Kodluyoruz, Patika.dev ekiplerine ve ayrıca değerli bilgilerini bize aktaran EnginDenizAlpman hocama emekleri için çok teşekkür ederim.
+
+Asıl şimdi konumuza gelirsek size anlatmak veya ispatlamak istediğim konu mutlak değer.
+
+Mutlak Değer
+Mutlak değer bir sayının 0 sayısına olan uzaklığına denir. Mutlak değer aslında uzaklık ile ilgili bir kavramdır. Örneğin 2; hem 2'nin hem de -2'nin mutlak değeridir. Bizim ölçeçeğimiz uzaklık ister pozitif(+) olsun ister negatif(-) değer olsun, mesafinin mutlak değeri her zaman pozitiflik olacaktır. Peki neden negatif sayıların değeri de pozitif oluyor? Hemen açıklayalım.
+
+Temel Kural: Mutlak değer her zaman pozitif olur.
+
+ |x|=>0
+x sayısının olası iki durum için kuralın sağlanıp sağlanmadığını kontrol edelim.
+Durum 1: x => 0. Bu durumda mutlak değerin içi ya pozitif olur yada sıfır olur.
+
+|x|=>0 
+
+x=>0
+ 
+X=>0 
+olduğu için eşitsizliğin sol tarafı sıfır yada pozitiftir, kural sağlanılmış oldu.
+
+Durum 2: x<0. Bu durumda mutlak değeri için negatif olmalıdır.
+
+|x|=>0
+
+-x=<0
+
+x>0
+olduğu için eşitliğin sol tarafı pozitiftir, dolayısıyla kural ispatlanmış olmaktadır.
